c74525256ca206018f804d86c231a141d84bf292
Joseph Gardi <jgardi@fb.com>
2017-06-28 17:07:13 -0700

    finshed minimum requiremnts

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
@@ -2,6 +2,7 @@ package com.codepath.apps.restclienttemplate;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +13,10 @@ import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.interactor.TwitterInteractor;
 import com.codepath.apps.restclienttemplate.model.Tweet;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 import javax.inject.Inject;
 
@@ -53,15 +57,9 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> 
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        Tweet tweet = twitterInteractor.getTweets().get(position);
-
-        holder.tvUsername.setText(tweet.getUser().getName());
-        holder.tvBody.setText(tweet.getText());
-
-        Glide.with(holder.context)
-                .load(tweet.getUser().getProfileImageUrl())
-                .into(holder.ivProfileImage);
+        holder.bind(twitterInteractor.getTweets().get(position));
     }
+
 
     @Override
     public int getItemCount() {
@@ -69,7 +67,7 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> 
     }
 
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    class ViewHolder extends RecyclerView.ViewHolder {
         Context context;
         @BindView(R.id.ivProfileImage)
         ImageView ivProfileImage;
@@ -77,12 +75,46 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> 
         TextView tvUsername;
         @BindView(R.id.tvBody)
         TextView tvBody;
+        @BindView(R.id.tvTime)
+        TextView tvTime;
 
 
-        public ViewHolder(View itemView) {
+        ViewHolder(View itemView) {
             super(itemView);
             context = itemView.getContext();
             ButterKnife.bind(this, itemView);
         }
+
+
+        public void bind(Tweet tweet) {
+            tvUsername.setText(tweet.getUser().getName());
+            tvBody.setText(tweet.getText());
+
+            Glide.with(context)
+                    .load(tweet.getUser().getProfileImageUrl())
+                    .into(ivProfileImage);
+
+            tvTime.setText(getRelativeTimeAgo(tweet.getCreatedAt()));
+        }
+
+        public String getRelativeTimeAgo(String rawJsonDate) {
+            String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+            SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+            sf.setLenient(true);
+
+            String relativeDate = "";
+            try {
+                long dateMillis = sf.parse(rawJsonDate).getTime();
+                relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                        System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            return relativeDate;
+        }
     }
+
+
+
 }
